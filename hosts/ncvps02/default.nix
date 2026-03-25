ef8d79bc63f7c40123e70e344e0ce4b0788e4371
tyrongabriel <51530686+tyrongabriel@users.noreply.github.com>
2026-03-25 18:45:19 +0100

    feat: onboarded ncvps02

---
+++ hosts/ncvps02/default.nix
@@ -0,0 +1,29 @@
+{ self, ... }:
+{
+  flake.modules.nixos.host_ncvps02 =
+    { lib, config, ... }:
+    {
+      imports = with self.modules.nixos; [
+        core
+        user_tyron
+        user_deploy
+      ];
+      config = {
+        networking.hostName = "ncvps02";
+        deployment = {
+          targetHost = "ncvps02.tail1c2108.ts.net";
+          targetUser = "tyron";
+        };
+        time.timeZone = lib.mkDefault "Europe/Vienna";
+
+        myNixos.users.tyron.homeManager = {
+          enable = true;
+          tags = [ "dev" ];
+        };
+
+        hardware.facter.reportPath = ./facter.json;
+        nixpkgs.system = "x86_64-linux";
+        system.stateVersion = "25.11";
+      };
+    };
+}
---
+++ hosts/ncvps02/networking.nix
@@ -0,0 +1,70 @@
+{ self, ... }:
+{
+  flake.modules.nixos.host_ncvps02 =
+    { ... }:
+    {
+      imports = [
+        self.modules.nixos.tailscale
+        
+      ];
+
+      networking.useDHCP = true;
+      #networking.useNetworkd = true;
+
+      # systemd.network.networks."10-wan" = {
+      #   matchConfig.Name = "eth0";
+      #   address = [
+      #     # v4
+      #     #     "159.195.9.89/22"
+      #     #     # v6
+      #     #     "2a0a:4cc0:ff:505::/64"
+      #     #   ];
+      #   routes = [
+      #     # v4
+      #     #     { Gateway = "159.195.8.1"; }
+      #     #     # v6
+      #     #     { Gateway = "fe80::1"; }
+      #     #   ];
+      #   networkConfig.DNS = [
+      #     #     "1.1.1.1"
+      #     #     "1.0.0.1"
+      #     #   ];
+      # };
+
+      networking.nameservers = [
+          "1.1.1.1"
+          "1.0.0.1"
+          
+        ];
+      networking.interfaces.eth0 = {
+      # Set this to false to ensure no DHCP is used for this interface
+      #useDHCP = true; # Cloud providers dont give dhcp!!
+        # Configure the IPv4 address and subnet mask
+        ipv4.addresses = [
+          {
+            address = "159.195.9.89";
+            prefixLength = 22;
+          }
+          
+        ];
+        # Configure the IPv6 address and prefix length
+        ipv6.addresses = [
+          {
+            address = "2a0a:4cc0:ff:505::";
+            prefixLength = 64;
+          }
+          
+        ];
+      };
+
+      # Set the default gateway
+      networking.defaultGateway = {
+        address = "159.195.8.1";
+        interface = "eth0";
+      };
+      networking.defaultGateway6 = {
+        address = "fe80::1";
+        interface = "eth0";
+      };
+    };
+}
