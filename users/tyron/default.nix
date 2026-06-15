ee4ed06c434c7a1e2aa43cdccbe3e4b2c677421a
tyrongabriel <51530686+tyrongabriel@users.noreply.github.com>
2026-06-15 20:52:41 +0200

    chore: added resient keys as ssh keys

--- users/tyron/default.nix
+++ users/tyron/default.nix
@@ -36,13 +36,9 @@ in
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqAq3GCuNXFc8mQL+H/czF0+pOlyQ4c4GILKUcrK0fZ 51530686+tyrongabriel@users.noreply.github.com"
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBY2DkFamhhD8nnS8zqCnJRMD2GKvmiV9QQk+1dfA/Z tyron@legion"
 
-            # Legion Tyron
-            "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBqmSrL50wt06SvVxM3e/BK62SeOQk37/qR7MWhC3lp1AAAABHNzaDo= tyron@legion" # yelen
-            "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIN/+Vj/gR3pP/Y1vctxiNOgOosnH3edSG6vTmxoPrCPxAAAABHNzaDo= tyron@legion" # yusuf
-
-            # Yoga
-            "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAII3eGSG9pGTvpXhFO2veBym9PSucRniOr4xQdis2gvkSAAAABHNzaDo= tyron@yoga"
-            "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIB91FdHgHdNIc6W6AKeSGSRpg9aNJkpXfCYL2Ogv9Iy2AAAABHNzaDo= tyron@yoga"
+            # Yubikeys
+            "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPsJ5MFDfXhkemPUaDDL2Ozkxj8m+90+HYs80om11q7ZAAAACXNzaDp5dXN1Zg== tyron@yusuf"
+            "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIC0zX/W36xFcPSeDnhJKZrOaWUeKIXkDFA+i/IZpRrXGAAAACXNzaDp5ZWxlbg== tyron@yelen"
           ];
         };
 
